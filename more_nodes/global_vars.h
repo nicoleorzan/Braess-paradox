5014b67ce8f29cfbdd176f2f9cd69afb754a1419
nicoleorzan <orzan.nicole@gmail.com>
2019-07-03 17:01:00 +0200

    working on runge kutta and capacity increasing plot

---
+++ more_nodes/global_vars.h
@@ -0,0 +1,9 @@
+#ifndef GLOBAL_VARIABLES
+#define GLOBAL_VARIABLES
+
+extern int connections;
+extern double alpha;
+extern double Gamma;
+extern double P[8];
+
+#endif
--- more_nodes/runge_kutta_adjlist_increasing_capacity.c
+++ more_nodes/runge_kutta_adjlist_increasing_capacity.c
@@ -2,20 +2,13 @@
 #include <stdio.h>
 #include <math.h>
 #include "global_vars.h"
-#include <sys/resource.h>
-#include <sys/times.h>
-#include <time.h>
 
 # define nodes 8
-
-#define TCPU_TIME (clock_gettime( CLOCK_PROCESS_CPUTIME_ID, &ts ), (double)ts.tv_sec +	\
-		   (double)ts.tv_nsec * 1e-9)
 
 int connections = 22;
 double alpha = 1;
 double Gamma = 0;
 double P[8] = {-1, 1, 1, 1, -1, -1, 1, -1};
-
 
 
 void print_mat(double *matrix, int N, int M){
@@ -47,15 +40,17 @@ int main(){
 
   int steps = 2500;
 
-  double tstart, tstop, ctime=0;
-  struct timespec ts;
-  int printing_step = 10;
+  int printing_step = 2400;
   double fR = 50;
   double wR = 2*M_PI*fR;
   double h = 0.01;
   double *theta = (double*) malloc(nodes * sizeof(double));
   double *omega = (double*) malloc(nodes * sizeof(double));
-  //double *somma = (double*) malloc(nodes * sizeof(double));
+
+  for (int i=0; i<nodes; i++){
+    theta[i] = 0;
+    omega[i] = 0;
+  }
   
   // reading from file
  
@@ -66,14 +61,13 @@ int main(){
   AVV = fopen("files_to_read/AV_adding_line.txt", "r");
   //AII = fopen("files_to_read/AI.txt", "r");
   AII = fopen("files_to_read/AI_adding_line.txt", "r");
-  W = fopen("files_to_read/weights_adding_line.txt", "r");
   //W = fopen("files_to_read/weights.txt", "r");
+  W = fopen("files_to_read/weights_adding_line.txt", "r");
 
   if (AII == NULL || AVV == NULL || W == NULL){
     printf("Error Reading File\n");
     exit(0);
   }
-
   for (int i = 0; i < connections; i++){
     fscanf(W, "%lf,", &weights[i] );
     fscanf(AVV, "%d,", &AV[i] );
@@ -85,61 +79,92 @@ int main(){
   fclose(AVV);
   fclose(AII);
   fclose(W);
-
   
-  // variables initialization
-  
-  for (int i=0; i<nodes; i++){
-    theta[i] = 0;
-    omega[i] = 0;
-  }
   
   // integration using runge-kutta method of 4th order
 
   FILE *theta_doc;
   double hh = h/0.5, h6 = h/6;
   double sum = 0;
-
+  double deltaK = 0.1;
+  
   theta_doc = fopen("theta", "w");
   
   double k1[2], k2[2], k3[2], k4[2];
 
-  double yt[8], dym[8], dyt[8], dydx[8];
+  //double yt[8], dym[8], dyt[8], dydx[8];
 
-  tstart = TCPU_TIME;
+  double cap = weights[0];
+  int iter = 0;
+  double max_capacity = 1.5;
+  
+  while (cap < max_capacity){
 
-  for (int t=1; t<=steps; t++){
-    fprintf(theta_doc, "%16.8f", t*h);
-
+    fprintf(theta_doc, "%16.8f", deltaK*iter);
+    
     for (int i=0; i<nodes; i++){
-
-      sum = 0;
-      for (int j = AI[i]; j < AI[i+1]; j++){
-	sum += weights[j] * sin( theta[i] - theta[(AV[j])] ); 
-      }
-      
-      k1[0] = omega_funct(omega[i], theta[i], sum, i);
-      k1[1] = theta_funct(omega[i]);
-      k2[0] = omega_funct(omega[i]+k1[0]*hh, theta[i], sum, i);
-      k2[1] = theta_funct(omega[i]+k1[1]*hh);
-      k3[0] = omega_funct(omega[i]+k2[0]*hh,  theta[i], sum, i);
-      k3[1] = theta_funct(omega[i]+k2[1]*hh);
-      k4[0] = omega_funct(omega[i]+k3[0]*h,  theta[i], sum, i);
-      k4[1] = theta_funct(omega[i]+k3[1]*h);
-
-      omega[i] = omega[i] + h6*(k1[0]+2*k2[0]+2*k3[0]+k4[0]);
-      theta[i] = theta[i] + h6*(k1[1]+2*k2[1]+2*k3[1]+k4[1]);
-      
-      
-      if (t % printing_step == 0){
-	fprintf(theta_doc, "%16.8e", theta[i]/M_PI);
-      }
+      theta[i] = 0;
+      omega[i] = 0;
     }
-    fprintf(theta_doc, "\n");
-  }
+   
+    for (int t=1; t<=steps; t++){
+      //fprintf(stdout, "%d\n", t);
+      //fprintf(theta_doc, "%16.8f", t*h);
 
-  ctime += TCPU_TIME - tstart;
-  printf("%g sec \n", ctime);
+      for (int i=0; i<nodes; i++){
+
+	sum = 0;
+
+	for (int j = AI[i]; j < AI[i+1]; j++){
+	  sum += weights[j] * sin( theta[i] - theta[(AV[j])] ); 
+	}
+      
+	k1[0] = omega_funct(omega[i], theta[i], sum, i);
+	k1[1] = theta_funct(omega[i]);
+	k2[0] = omega_funct(omega[i]+k1[0]*hh, theta[i], sum, i);
+	k2[1] = theta_funct(omega[i]+k1[1]*hh);
+	k3[0] = omega_funct(omega[i]+k2[0]*hh,  theta[i], sum, i);
+	k3[1] = theta_funct(omega[i]+k2[1]*hh);
+	k4[0] = omega_funct(omega[i]+k3[0]*h,  theta[i], sum, i);
+	k4[1] = theta_funct(omega[i]+k3[1]*h);
+
+	omega[i] = omega[i] + h6*(k1[0]+2*k2[0]+2*k3[0]+k4[0]);
+	theta[i] = theta[i] + h6*(k1[1]+2*k2[1]+2*k3[1]+k4[1]);
+      
+      
+	
+	/*if (t % printing_step == 0){
+	  fprintf(theta_doc, "%16.8e", theta[i]/M_PI);
+	  }
+	*/
+      }
+      if (t == printing_step){
+	fprintf(theta_doc, "%16.8e", (theta[3]-theta[4])/M_PI); //diff nodes 4-5
+	fprintf(theta_doc, "%16.8e", (theta[3]-theta[7])/M_PI); //diff nodes 4-8
+	fprintf(theta_doc, "%16.8e", (theta[0]-theta[5])/M_PI); //diff nodes 1-6
+	fprintf(theta_doc, "%16.8e", (theta[2]-theta[3])/M_PI); //diff nodes 3-4
+	fprintf(theta_doc, "%16.8e", (theta[2]-theta[1])/M_PI); //diff nodes 3-2
+	fprintf(theta_doc, "%16.8e", (theta[2]-theta[6])/M_PI); //diff nodes 3-7
+	fprintf(theta_doc, "%16.8e", (theta[5]-theta[4])/M_PI); //diff nodes 6-5
+	fprintf(theta_doc, "%16.8e", (theta[5]-theta[7])/M_PI); //diff nodes 6-8
+	fprintf(theta_doc, "%16.8e", (theta[1]-theta[0])/M_PI); //diff nodes 2-1
+	fprintf(theta_doc, "%16.8e", (theta[6]-theta[0])/M_PI); //diff nodes 7-1
+      }
+      
+      //fprintf(theta_doc, "\n");
+    }
+
+    
+    
+    weights[5] += deltaK;
+    weights[8] += deltaK;
+    iter+=1;
+    cap += deltaK;
+
+    fprintf(theta_doc, "\n");
+    
+  }
+   
 
   fclose(theta_doc);
   free(theta);  free(omega);
