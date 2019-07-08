67272cb181174f7b70b77f30bfa9715f19759b75
nicoleorzan <orzan.nicole@gmail.com>
2019-07-08 16:56:23 +0200

    cleaning

--- more_nodes/General_case/varying_control_runge_kutta_general.c
+++ more_nodes/General_case/varying_control_runge_kutta_general.c
@@ -14,7 +14,17 @@
 #define delta_step 0.01
 #define delta_min 0.0
 
-void stability_check(double* y, bool *unstable, FILE* control_variation){
+void print_info(FILE *file){
+  fprintf(file, "Usual network with 8 nodes\n");
+  fprintf(file, "control form: Pmax*tanh(delta*theta)\n");
+  fprintf(file, "Pmax = %f, variation of delta from %f to %f with step %f\n", Pmax, delta, delta_min, delta_step);
+  fprintf(file, "Computation of stability reached/unreached in every variation of delta with comparison after %i and %i steps\n", steps, steps+additive_steps);
+  fprintf(file, "Computation is stopped when stability is not reached anymore\n");
+  fprintf(file, "Error is computed as sum_i ( |(theta_%i[i] - theta_%i[i]| ) \n", steps, steps+additive_steps);
+  fprintf(file, "Stability is not reached if error >= %16.8e\n\n", max_error);
+}
+
+void stability_check(double* y, bool *unstable, FILE* file){
   
   double theta_save[nodes];
   double error[nodes];
@@ -32,15 +42,15 @@ void stability_check(double* y, bool *unstable, FILE* control_variation){
     sum += error[i];
   }
   if (sum >= max_error) {
-    fprintf(control_variation, "delta =%16.8e\n", delta);
-    fprintf(control_variation, "error =%16.8e\n", sum);
-    fprintf(control_variation, "Stability not reached\n\n");
+    fprintf(file, "delta =%16.8e\n", delta);
+    fprintf(file, "error =%16.8e\n", sum);
+    fprintf(file, "Stability not reached\n\n");
     *unstable = 1;
   }
   else {
-    fprintf(control_variation, "delta =%16.8e\n", delta);
-    fprintf(control_variation, "error =%16.8e\n", sum);
-    fprintf(control_variation, "Stability reached\n\n");
+    fprintf(file, "delta =%16.8e\n", delta);
+    fprintf(file, "error =%16.8e\n", sum);
+    fprintf(file, "Stability reached\n\n");
   }
 }
 
@@ -53,17 +63,11 @@ int main(){
   
   double *y = (double*) malloc(2 * nodes * sizeof(double));
 
-  FILE* control_variation;
-  control_variation = fopen("control_variation", "w");
+  FILE* file;
+  file = fopen("control_variation", "w");
 
-  fprintf(control_variation, "Usual network with 8 nodes\n");
-  fprintf(control_variation,"control form: Pmax*tanh(delta*theta)\n");
-  fprintf(control_variation,"Pmax = %f, variation of delta from %f to %f with step %f\n", Pmax, delta, delta_min, delta_step);
-  fprintf(control_variation, "Computation of stability reached/unreached in every variation of delta with comparison after %i and %i steps\n", steps, steps+additive_steps);
-  fprintf(control_variation, "Computation is stopped when stability is not reached anymore\n");
-  fprintf(control_variation, "Error is computed as sum_i ( |(theta_%i[i] - theta_%i[i]| ) \n", steps, steps+additive_steps);
-  fprintf(control_variation, "Stability is not reached if error >= %16.8e\n\n", max_error);
-
+  print_info(file);
+ 
   tstart = TCPU_TIME;
 
   while (delta > delta_min){
@@ -76,7 +80,7 @@ int main(){
       runge_kutta(y, internal_steps);
     }
     
-    stability_check(y, &unstable, control_variation);
+    stability_check(y, &unstable, file);
     
     if (unstable == 1) break;
     delta = delta - delta_step;
@@ -85,14 +89,12 @@ int main(){
   ctime += TCPU_TIME - tstart;
   printf("%g sec \n", ctime);
 
-  fclose(control_variation);
-  memset(control_variation, 0, sizeof(*control_variation));
-  free(control_variation);
+  fclose(file);
+  memset(file, 0, sizeof(*file));
+  free(file);
   memset(y, 0, sizeof(*y));
   free(y);
   
   return 0;
 }
-
-
 
