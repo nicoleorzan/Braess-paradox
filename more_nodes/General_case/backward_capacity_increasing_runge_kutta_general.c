79dae3b62bb317c1c818380ab75661849bb1e787
nicoleorzan <orzan.nicole@gmail.com>
2019-07-09 15:21:15 +0200

    backward code

---
+++ more_nodes/General_case/backward_capacity_increasing_runge_kutta_general.c
@@ -0,0 +1,122 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "../include/time_computing.h"
+#include "../include/network.h"
+#include "../include/runge_kutta.h"
+
+#define steps 50000
+#define additive_steps 1000
+#define internal_steps 10
+#define max_error 10e-9
+
+#define min_capacity 1.03
+#define deltaK 0.01
+
+
+void stability_check(double* y){
+    
+  double theta_save[nodes];
+  double error[nodes];
+  double sum = 0.;
+  
+  for (int i=0; i<nodes; i++){
+    theta_save[i] = y[i];
+    error[i] = 0.;
+  }
+
+  runge_kutta(y, additive_steps);
+  
+  for (int i=0; i<nodes; i++){
+    error[i] = fabs(theta_save[i] - y[i]);
+    sum += error[i];
+  }
+  if (sum >= max_error) {
+    fprintf(stdout, "error =%16.8e\n", sum);
+    fprintf(stdout, "Stability not reached\n\n");
+  }
+  else {
+    fprintf(stdout, "error =%16.8e\n", sum);
+    fprintf(stdout, "Stability reached\n\n");
+  }
+}
+
+void printer(double * y, FILE * f){
+  fprintf(f, "%16.8e", (y[3]-y[4])/M_PI); //diff nodes 4-5
+  fprintf(f, "%16.8e", (y[3]-y[7])/M_PI); //diff nodes 4-8
+  fprintf(f, "%16.8e", (y[0]-y[5])/M_PI); //diff nodes 1-6
+  fprintf(f, "%16.8e", (y[2]-y[3])/M_PI); //diff nodes 3-4
+  fprintf(f, "%16.8e", (y[2]-y[1])/M_PI); //diff nodes 3-2
+  fprintf(f, "%16.8e", (y[2]-y[6])/M_PI); //diff nodes 3-7
+  fprintf(f, "%16.8e", (y[5]-y[4])/M_PI); //diff nodes 6-5
+  fprintf(f, "%16.8e", (y[5]-y[7])/M_PI); //diff nodes 6-8
+  fprintf(f, "%16.8e", (y[1]-y[0])/M_PI); //diff nodes 2-1
+  fprintf(f, "%16.8e", (y[6]-y[0])/M_PI); //diff nodes 7-1
+  fprintf(f, "\n");
+}
+
+
+int main(){
+
+  double tstart, tstop, ctime=0;
+  struct timespec ts;
+  
+  double *y = (double*) malloc(2 * nodes * sizeof(double));
+  int iter = 0;
+  weights[5] = 1.68;
+  weights[8] = 1.68;
+  double cap = 1.68;
+
+  FILE* capacity_doc;
+  capacity_doc = fopen("increasing_capacity", "w");
+
+  tstart = TCPU_TIME;
+
+  y[0] = 1.43882594e+02;
+  y[1] = 1.44826774e+02;
+  y[2] = 1.44641432e+021;
+  y[3] = 1.43963977e+02;
+  y[4] = -2.40778324e+02;
+  y[5] = -2.40584902e+02;
+  y[6] = 1.44826774e+02;
+  y[7] = -2.40778324e+02;
+  y[8] = 1.50343924e-01;
+  y[9] = 1.92666281e-01;
+  y[10] = 2.64488778e-01;
+  y[11] = -1.93667270e-01;
+  y[12] = -3.84146548e-01;
+  y[13] = 1.61795102e-01;
+  y[14] = -3.84146548e-01;
+  y[15] = -3.84146548e-01;
+ 
+  while (cap > min_capacity){
+    fprintf(stdout, "%f\n", cap);
+    fprintf(capacity_doc, "%16.8f", deltaK*iter);
+
+    for (int t=1; t<=steps; t+=internal_steps){  
+      runge_kutta(y, internal_steps);
+    }
+    stability_check(y);
+
+    printer(y, capacity_doc);
+
+    weights[5] -= deltaK;
+    weights[8] -= deltaK;
+    iter += 1;
+    cap -= deltaK;
+  }
+
+  ctime += TCPU_TIME - tstart;
+  printf("%g sec \n", ctime);
+
+  fclose(capacity_doc);
+  memset(capacity_doc, 0, sizeof(*capacity_doc));
+  free(capacity_doc);
+  memset(y, 0, sizeof(*y));
+  free(y);
+    
+  return 0;
+}
+
+
+
